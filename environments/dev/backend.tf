58861a88016f4989422f575bc780db91ce495b82
ljanaideh <ljanaideh@atypon.com>
2026-04-07 12:09:00 +0300

    update the S3 bucket name

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "react-app-tfstate"
+    bucket         = "react-app-tfstate-laith"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
