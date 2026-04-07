c68269e31563a4e07853253d7aed4b31a0cde603
ljanaideh <ljanaideh@atypon.com>
2026-04-07 13:17:01 +0300

    force ec2 replace with arm image

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -58,6 +58,7 @@ resource "aws_spot_instance_request" "app" {
 
   user_data = base64encode(<<-EOF
     #!/bin/bash
+    # v2 - arm image
     set -ex
     dnf install -y docker
     systemctl enable docker && systemctl start docker
