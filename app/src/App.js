110d104ca73a31e355a525fcf1d0d91659a39bcd
ljanaideh <ljanaideh@atypon.com>
2026-04-07 11:41:11 +0300

    add terraform infra + react app + atlantis config

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>React on AWS EC2</h1>
+      <p>Deployed with Terraform + Atlantis</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ environments/dev/outputs.tf
@@ -0,0 +1,7 @@
+output "public_ip" {
+  value = module.ec2.public_ip
+}
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
