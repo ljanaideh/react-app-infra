211caa8d42ab456dad374157b3a8b059f9c0e9d7
ljanaideh <ljanaideh@atypon.com>
2026-04-07 12:30:36 +0300

    fix backend config

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "react-app-tfstate-laith"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-locks"
+    bucket       = "react-app-tfstate-laith"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
